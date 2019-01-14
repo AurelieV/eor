fe8a3a6b79e100e6d0ec5c965815cfe1d5c3af22
Aurélie VIOLETTE <orwel.violette@gmail.com>
2019-01-14 15:03:12 +0100

    Adds firebase configuration + trigger onDelete

---
+++ functions/src/index.ts
@@ -0,0 +1,19 @@
+import * as admin from 'firebase-admin'
+import * as functions from 'firebase-functions'
+
+admin.initializeApp(functions.config().firebase)
+
+const tournamentFields = ['zones', 'staff', 'zoneTables']
+exports.deleteTournament = functions.database
+  .ref('/tournaments/{id}')
+  .onDelete((snapshot, { params }) => {
+    const id = params.id
+    return Promise.all(
+      tournamentFields.map((field) =>
+        admin
+          .database()
+          .ref(`/${field}/${id}`)
+          .remove()
+      )
+    )
+  })
